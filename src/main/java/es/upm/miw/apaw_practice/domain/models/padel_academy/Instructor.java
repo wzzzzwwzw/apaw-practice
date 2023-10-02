1b1a720a12b000901b7e6abada8875471456744c
Michael Hernandez <michaelhernandezrala@gmail.com>
2023-10-02 22:24:26 +0200

    Delete instructor-lesson relation #476

--- src/main/java/es/upm/miw/apaw_practice/domain/models/padel_academy/Instructor.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/padel_academy/Instructor.java
@@ -1,19 +1,15 @@
 package es.upm.miw.apaw_practice.domain.models.padel_academy;
 
-import java.time.LocalDate;
-import java.util.List;
 
 public class Instructor {
     private String dni;
     private String name;
     private Integer phoneNumber;
-    private List<Lesson> lessons;
 
-    public Instructor(String dni, String name, Integer phoneNumber, List<Lesson> lessons) {
+    public Instructor(String dni, String name, Integer phoneNumber) {
         this.dni = dni;
         this.name = name;
         this.phoneNumber = phoneNumber;
-        this.lessons = lessons;
     }
 
     public String getDni() {
@@ -40,21 +36,12 @@ public class Instructor {
         this.phoneNumber = phoneNumber;
     }
 
-    public List<Lesson> getLessons() {
-        return lessons;
-    }
-
-    public void setLessons(List<Lesson> lessons) {
-        this.lessons = lessons;
-    }
-
     @Override
     public String toString() {
         return "Instructor{" +
                 "dni='" + dni + '\'' +
                 ", name='" + name + '\'' +
                 ", phoneNumber=" + phoneNumber +
-                ", lessons=" + lessons +
                 '}';
     }
 }

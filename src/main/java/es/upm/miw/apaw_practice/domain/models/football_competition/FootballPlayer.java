1d2b32dcf77ded4a3173c8391e7d642fa10701ca
ruliworst <raulfa1503@gmail.com>
2023-10-02 22:52:36 +0200

    Added FootballPlayer class #481

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/football_competition/FootballPlayer.java
@@ -0,0 +1,62 @@
+package es.upm.miw.apaw_practice.domain.models.football_competition;
+
+import java.math.BigDecimal;
+
+public class FootballPlayer {
+    String name;
+    BigDecimal salary;
+    Float height;
+    Integer goals;
+    Integer assists;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public Float getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+    public Integer getGoals() {
+        return this.goals;
+    }
+
+    public void setGoals(Integer goals) {
+        this.goals = goals;
+    }
+
+    public Integer getAssists() {
+        return this.assists;
+    }
+
+    public void setAssists(Integer assists) {
+        this.assists = assists;
+    }
+
+    @Override
+    public String toString() {
+        return "FootballPlayer{" +
+                "Name=" + this.name +
+                ", Salary=" + this.salary +
+                ", Height=" + this.height + "cm" +
+                ", Goals=" + this.goals +
+                ", Assists=" + this.assists +
+                '}';
+    }
+}

be88e8ff183702e5fa7cc8e0df0903ef256a6a52
Michael Hernandez <michaelhernandezrala@gmail.com>
2023-10-03 01:06:43 +0200

    Diagram and model changes #476

--- src/main/java/es/upm/miw/apaw_practice/domain/models/padel_academy/Court.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/padel_academy/Court.java
@@ -1,19 +1,14 @@
 package es.upm.miw.apaw_practice.domain.models.padel_academy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Court {
     private Integer number;
     private Boolean withRoof;
     private String surface;
-    private List<Lesson> lessons;
 
     public Court(Integer number, Boolean withRoof, String surface) {
         this.number = number;
         this.withRoof = withRoof;
         this.surface = surface;
-        this.lessons = new ArrayList<Lesson>();
     }
 
     public Integer getNumber() {
@@ -40,21 +35,12 @@ public class Court {
         this.surface = surface;
     }
 
-    public List<Lesson> getLessons() {
-        return lessons;
-    }
-
-    public void setLessons(List<Lesson> lessons) {
-        this.lessons = lessons;
-    }
-
     @Override
     public String toString() {
         return "Court{" +
                 "number=" + number +
                 ", withRoof=" + withRoof +
                 ", surface='" + surface + '\'' +
-                ", lessons=" + lessons +
                 '}';
     }
 }

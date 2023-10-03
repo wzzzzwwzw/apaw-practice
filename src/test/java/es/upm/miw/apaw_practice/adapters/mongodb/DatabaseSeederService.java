d805bb785f127ba852e5c3c265a4f1d2bc91509f
adriancastrovilar <adrian.castrov@alumnos.upm.es>
2023-10-03 20:36:13 +0200

    Added repository tests and RestaurantSeederService #482

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.computer_store.ComputerStoreSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.restaurant.RestaurantSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.zoo.ZooSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +13,15 @@ public class DatabaseSeederService {
     private final ShopSeederService shopSeederService;
     private final ZooSeederService zooSeederService;
     private final ComputerStoreSeederService computerStoreSeederService;
+    private final RestaurantSeederService restaurantSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService, ComputerStoreSeederService computerStoreSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService, ComputerStoreSeederService computerStoreSeederService,
+                                 RestaurantSeederService restaurantSeederService) {
         this.shopSeederService = shopSeederService;
         this.zooSeederService = zooSeederService;
         this.computerStoreSeederService = computerStoreSeederService;
+        this.restaurantSeederService = restaurantSeederService;
         this.seedDatabase();
     }
 
@@ -25,12 +29,14 @@ public class DatabaseSeederService {
         this.shopSeederService.seedDatabase();
         this.zooSeederService.seedDatabase();
         this.computerStoreSeederService.seedDatabase();
+        this.restaurantSeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
         this.zooSeederService.deleteAll();
         this.computerStoreSeederService.deleteAll();
+        this.restaurantSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {

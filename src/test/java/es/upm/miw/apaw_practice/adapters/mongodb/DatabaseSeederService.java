f0995a29e4fa4d2b0c315579264a599b58374b4f
Martahc <martahica@gmail.com>
2023-10-04 18:33:44 +0200

    added entities, repositories and tests #490

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -3,9 +3,11 @@ package es.upm.miw.apaw_practice.adapters.mongodb;
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.CarWorkshopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.computer_store.ComputerStoreSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.conference.ConferenceSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.FoodDeliverySeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.restaurant.RestaurantSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.zoo.ZooSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.FoodDeliverySeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +20,20 @@ public class DatabaseSeederService {
     private final ConferenceSeederService conferenceSeederService;
     private final RestaurantSeederService restaurantSeederService;
     private final CarWorkshopSeederService carWorkshopSeederService;
+    private final FoodDeliverySeederService foodDeliverySeederService;
 
     @Autowired
     public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService,
             ComputerStoreSeederService computerStoreSeederService, ConferenceSeederService conferenceSeederService,
-            RestaurantSeederService restaurantSeederService, CarWorkshopSeederService carWorkshopSeederService) {
+            RestaurantSeederService restaurantSeederService, CarWorkshopSeederService carWorkshopSeederService,
+                                 FoodDeliverySeederService foodDeliverySeederService) {
         this.shopSeederService = shopSeederService;
         this.zooSeederService = zooSeederService;
         this.computerStoreSeederService = computerStoreSeederService;
         this.conferenceSeederService = conferenceSeederService;
         this.restaurantSeederService = restaurantSeederService;
         this.carWorkshopSeederService = carWorkshopSeederService;
+        this.foodDeliverySeederService = foodDeliverySeederService;
         this.seedDatabase();
     }
 
@@ -39,6 +44,7 @@ public class DatabaseSeederService {
         this.conferenceSeederService.seedDatabase();
         this.restaurantSeederService.seedDatabase();
         this.carWorkshopSeederService.seedDatabase();
+        this.foodDeliverySeederService.seedDatabase();
     }
 
     public void deleteAll() {
@@ -48,6 +54,7 @@ public class DatabaseSeederService {
         this.conferenceSeederService.deleteAll();
         this.restaurantSeederService.deleteAll();
         this.carWorkshopSeederService.deleteAll();
+        this.foodDeliverySeederService.deleteAll();
     }
 
     public void reSeedDatabase() {

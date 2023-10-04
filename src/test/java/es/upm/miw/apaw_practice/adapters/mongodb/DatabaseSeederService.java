879ad64961b1f952d77b54400167e82a35cd4b87
Mario <mario10_bd@hotmail.com>
2023-10-04 09:11:34 +0200

    added seeder service Car Workship persistence desing #488

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.CarWorkshopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.computer_store.ComputerStoreSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.conference.ConferenceSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.restaurant.RestaurantSeederService;
@@ -16,15 +17,18 @@ public class DatabaseSeederService {
     private final ComputerStoreSeederService computerStoreSeederService;
     private final ConferenceSeederService conferenceSeederService;
     private final RestaurantSeederService restaurantSeederService;
+    private final CarWorkshopSeederService carWorkshopSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService, ComputerStoreSeederService computerStoreSeederService, ConferenceSeederService conferenceSeederService,
-                                 RestaurantSeederService restaurantSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService,
+            ComputerStoreSeederService computerStoreSeederService, ConferenceSeederService conferenceSeederService,
+            RestaurantSeederService restaurantSeederService, CarWorkshopSeederService carWorkshopSeederService) {
         this.shopSeederService = shopSeederService;
         this.zooSeederService = zooSeederService;
         this.computerStoreSeederService = computerStoreSeederService;
         this.conferenceSeederService = conferenceSeederService;
         this.restaurantSeederService = restaurantSeederService;
+        this.carWorkshopSeederService = carWorkshopSeederService;
         this.seedDatabase();
     }
 
@@ -34,6 +38,7 @@ public class DatabaseSeederService {
         this.computerStoreSeederService.seedDatabase();
         this.conferenceSeederService.seedDatabase();
         this.restaurantSeederService.seedDatabase();
+        this.carWorkshopSeederService.seedDatabase();
     }
 
     public void deleteAll() {
@@ -42,6 +47,7 @@ public class DatabaseSeederService {
         this.computerStoreSeederService.deleteAll();
         this.conferenceSeederService.deleteAll();
         this.restaurantSeederService.deleteAll();
+        this.carWorkshopSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {

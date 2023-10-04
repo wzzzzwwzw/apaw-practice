62d3f92ae298ff05f71a08efc636faa85eabf9d5
Mario <mario10_bd@hotmail.com>
2023-10-04 08:34:44 +0200

    added daos Car Workship persistence desing #488

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/CarToRepairRepository.java
@@ -0,0 +1,9 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.CarToRepairEntity;
+
+public interface CarToRepairRepository extends MongoRepository<CarToRepairEntity, String> {
+
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OBDFaultRepository.java
@@ -0,0 +1,9 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OBDFaultEntity;
+
+public interface OBDFaultRepository extends MongoRepository<OBDFaultEntity, String> {
+
+}

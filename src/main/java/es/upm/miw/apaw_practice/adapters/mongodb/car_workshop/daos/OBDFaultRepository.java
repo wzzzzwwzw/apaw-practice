9ebdb138e11d72a3f77423ac04785870b5d5b427
Mario <mario10_bd@hotmail.com>
2023-10-04 09:07:20 +0200

    added 2 methods to DAOS Car Workship persistence desing #488

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OBDFaultRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OBDFaultRepository.java
@@ -1,9 +1,12 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OBDFaultEntity;
 
 public interface OBDFaultRepository extends MongoRepository<OBDFaultEntity, String> {
 
+    Optional<OBDFaultEntity> findBYCode(String code);
 }

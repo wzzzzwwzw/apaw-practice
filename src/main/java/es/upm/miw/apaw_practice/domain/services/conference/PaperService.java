ddeef60497e1809008fd4846ee32095724462f3c
JorgeDSDP <jorge.dasilva@alumni.mondragon.edu>
2023-10-07 19:20:30 +0200

    PaperResource created and implemented #512

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/conference/PaperService.java
@@ -0,0 +1,9 @@
+package es.upm.miw.apaw_practice.domain.services.conference;
+
+import es.upm.miw.apaw_practice.domain.models.conference.Paper;
+
+public class PaperService {
+    public Paper read(String digitalObjectIdentifier) {
+        return null;
+    }
+}

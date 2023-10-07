ce0cac4084b88c968a6d0d32ee0eb21ae24a10b2
Diego Guerrero <diego.guerreroc@alumnos.upm.es>
2023-10-07 19:20:25 +0200

    Museum - Creates ArtWork resource and service scaffolding #536

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/museum/ArtWorkService.java
@@ -0,0 +1,22 @@
+package es.upm.miw.apaw_practice.domain.services.museum;
+
+import es.upm.miw.apaw_practice.domain.models.museum.ArtWork;
+import es.upm.miw.apaw_practice.domain.persistence_ports.museum.ArtWorkPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArtWorkService {
+
+    private final ArtWorkPersistence artWorkPersistence;
+
+    @Autowired
+    public ArtWorkService(ArtWorkPersistence artWorkPersistence) {
+        this.artWorkPersistence = artWorkPersistence;
+    }
+
+    public ArtWork findByInventoryNumber(String inventoryNumber) {
+        // TODO
+        return null;
+    }
+}

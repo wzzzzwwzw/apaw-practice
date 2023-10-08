53f2d94f1e1e130d2b34cc78382646e049a9b97f
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2023-10-08 17:43:27 +0200

    Zoo DELETE ZOO Resource #530

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/zoo/ZooService.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw_practice.domain.services.zoo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ZooService {
+
+
+    public void delete(String name) {
+        //TODO
+    }
+}

8b047ce7cc07dd13c2e78de0242084dee4cdf4ab
ruliworst <raulfa1503@gmail.com>
2023-10-08 15:57:18 +0200

    Added FootballCompetitionEntity and its repository #487

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/football_competition/daos/FootballCompetitionRepository.java
@@ -0,0 +1,9 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.football_competition.daos;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.football_competition.entities.FootballCompetitionEntity;
+
+import java.util.Optional;
+
+public interface FootballCompetitionRepository {
+    Optional<FootballCompetitionEntity> findById(String id);
+}

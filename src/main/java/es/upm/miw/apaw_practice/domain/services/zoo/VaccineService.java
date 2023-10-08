3a7e504cab9aefb6f945cf273332e4829c3e8a4f
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2023-10-08 08:40:39 +0200

    Zoo GET Vaccine Resource #522

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/zoo/VaccineService.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw_practice.domain.services.zoo;
+
+import es.upm.miw.apaw_practice.domain.models.zoo.Vaccine;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VaccineService {
+    public Vaccine findByIdentifierBatch(String identifierBatch) {
+        //TODO
+        return null;
+    }
+}

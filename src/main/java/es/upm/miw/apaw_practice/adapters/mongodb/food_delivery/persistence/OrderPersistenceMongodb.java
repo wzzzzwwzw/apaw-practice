b318530967b1ac86ab6ae72323af5adfb8e119a0
Martahc <martahica@gmail.com>
2023-10-08 16:54:05 +0200

    added order persistence #546

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/food_delivery/persistence/OrderPersistenceMongodb.java
@@ -0,0 +1,27 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.daos.OrderRepository;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.models.food_delivery.Order;
+import es.upm.miw.apaw_practice.domain.persistence_ports.food_delivery.OrderPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Repository("orderPersistence")
+public class OrderPersistenceMongodb implements OrderPersistence {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderPersistenceMongodb(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+    @Override
+    public Order read(Integer number) {
+        return this.orderRepository.findByNumber(number)
+                .orElseThrow(() -> new NotFoundException("Order number: " + number))
+                .toOrder();
+    }
+}

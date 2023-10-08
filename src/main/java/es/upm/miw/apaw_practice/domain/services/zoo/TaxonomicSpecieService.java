9169ba5c7019792f860d1abb35c6af3102cc6066
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2023-10-08 10:44:42 +0200

    Zoo POST TAXONOMIC SPECIE Resource and create file service #525

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/zoo/TaxonomicSpecieService.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw_practice.domain.services.zoo;
+
+import es.upm.miw.apaw_practice.domain.models.zoo.TaxonomicSpecie;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaxonomicSpecieService {
+    public TaxonomicSpecie create(TaxonomicSpecie taxonomicSpecie) {
+        //TODO
+        return null;
+    }
+}

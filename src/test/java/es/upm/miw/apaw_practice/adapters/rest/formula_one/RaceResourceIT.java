bc297705026dabcd4c1c680dcbb842be45cb8c48
Oliver Fernandez <oliver.fernandez@alumnos.upm.es>
2023-10-08 13:05:38 +0200

    Formula one GET Race Test rest #505

---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/formula_one/RaceResourceIT.java
@@ -0,0 +1,53 @@
+package es.upm.miw.apaw_practice.adapters.rest.formula_one;
+
+import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.formula_one.Race;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+
+import static es.upm.miw.apaw_practice.adapters.rest.formula_one.RaceResource.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RestTestConfig
+public class RaceResourceIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testFindByCircuitName() {
+        this.webTestClient
+                .get()
+                .uri(RACES+CIRCUIT_NAME_ID, "Albert Park")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Race.class)
+                .value(Assertions::assertNotNull)
+                .value(race -> {
+                    assertEquals("Albert Park", race.getCircuitName());
+                    assertEquals("Australia", race.getHostCountry());
+                    assertEquals(LocalDate.of(2023, 4, 2), race.getDate());
+                    assertTrue(
+                            race.getRaceDrivers().stream()
+                                    .anyMatch(driver ->
+                                            driver.getDriverName().equals("Lance Stroll") &&
+                                                    driver.getNumber() == 18 &&
+                                                    driver.getNationality().equals("Canada")
+                                    )
+                    );
+                });
+    }
+
+    @Test
+    void testFindByCircuitNameNotFound() {
+        this.webTestClient
+                .get()
+                .uri(RACES+CIRCUIT_NAME_ID, "Imola")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+}

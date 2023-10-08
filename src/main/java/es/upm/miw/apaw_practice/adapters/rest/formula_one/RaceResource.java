8fac7a3af9389ae6c356740d05332552451f1c40
Oliver Fernandez <oliver.fernandez@alumnos.upm.es>
2023-10-08 11:58:57 +0200

    Formula one GET Race resource #505

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/formula_one/RaceResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/formula_one/RaceResource.java
@@ -1,8 +1,10 @@
 package es.upm.miw.apaw_practice.adapters.rest.formula_one;
 
+import es.upm.miw.apaw_practice.domain.models.formula_one.Race;
 import es.upm.miw.apaw_practice.domain.services.formula_one.RaceService;
-import es.upm.miw.apaw_practice.domain.services.restaurant.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class RaceResource {
 
     static final String RACES = "/formula-one/races";
+    static final String CIRCUIT_NAME_ID = "/{circuitName}";
 
     private final RaceService raceService;
 
     @Autowired
     public RaceResource(RaceService raceService) {
         this.raceService = raceService;
+    }
+
+    @GetMapping(CIRCUIT_NAME_ID)
+    public Race findByCircuitName(@PathVariable String circuitName) {
+        return this.raceService.findByCircuitName(circuitName);
     }
 }
--- src/main/java/es/upm/miw/apaw_practice/domain/services/formula_one/RaceService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/formula_one/RaceService.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.domain.services.formula_one;
 
+import es.upm.miw.apaw_practice.domain.models.formula_one.Race;
 import es.upm.miw.apaw_practice.domain.persistence_ports.formula_one.RacePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,8 @@ public class RaceService {
     @Autowired
     public RaceService(RacePersistence racePersistence) {
         this.racePersistence = racePersistence;
+    }
+
+    public Race findByCircuitName(String circuitName) {
     }
 }

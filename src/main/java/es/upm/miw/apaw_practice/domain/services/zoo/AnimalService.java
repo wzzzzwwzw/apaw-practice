0ff090441c64e07667bcd544f792c1937e995daf
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2023-10-08 12:14:17 +0200

    Zoo PUT ANIMAL AGE #527

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/zoo/AnimalService.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw_practice.domain.services.zoo;
+
+import es.upm.miw.apaw_practice.domain.models.zoo.Animal;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimalService {
+    public Animal updateAge(String identificationChip, Integer age) {
+        //TODO
+        return null;
+    }
+}

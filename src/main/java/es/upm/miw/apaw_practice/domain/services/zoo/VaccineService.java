3ff62aa099d14b724966ab90f990519c0e46a37a
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2023-10-08 08:46:15 +0200

    Zoo GET Vaccine Service and Interface Persistence #522

--- src/main/java/es/upm/miw/apaw_practice/domain/services/zoo/VaccineService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/zoo/VaccineService.java
@@ -1,12 +1,20 @@
 package es.upm.miw.apaw_practice.domain.services.zoo;
 
 import es.upm.miw.apaw_practice.domain.models.zoo.Vaccine;
+import es.upm.miw.apaw_practice.domain.persistence_ports.zoo.VaccinePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VaccineService {
+    private final VaccinePersistence vaccinePersistence;
+
+    @Autowired
+    public VaccineService(VaccinePersistence vaccinePersistence) {
+        this.vaccinePersistence = vaccinePersistence;
+    }
+
     public Vaccine findByIdentifierBatch(String identifierBatch) {
-        //TODO
-        return null;
+        return this.vaccinePersistence.findByIdentifierBatch(identifierBatch);
     }
 }

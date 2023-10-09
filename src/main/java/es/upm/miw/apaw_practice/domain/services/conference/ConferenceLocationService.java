4c62d93e271ea85982e103065acaf30f26c4af51
JorgeDSDP <jorge.dasilva@alumni.mondragon.edu>
2023-10-09 23:35:02 +0200

    ConferenceLocationResource for PATCH endpoint #514

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/conference/ConferenceLocationService.java
@@ -0,0 +1,9 @@
+package es.upm.miw.apaw_practice.domain.services.conference;
+
+import es.upm.miw.apaw_practice.domain.models.conference.ConferenceLocation;
+
+public class ConferenceLocationService {
+    public ConferenceLocation updateHall(String id, String hall) {
+        return null;
+    }
+}

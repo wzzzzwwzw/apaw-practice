c147c15a6de2b56e4d4c3a4d9d280a3cb58d0811
David Yareth Macaya Albericio <davidyareth@gmail.com>
2023-10-09 18:07:06 +0200

    add functions to AreaRepository, ClimberRepository & ExpeditionRepository #494

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/climbing/daos/AreaRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/climbing/daos/AreaRepository.java
@@ -3,5 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.climbing.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.climbing.entities.AreaEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface AreaRepository extends MongoRepository<AreaEntity, String> {
+    Optional<AreaEntity> findByName(String name);
 }
--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/climbing/daos/ClimberRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/climbing/daos/ClimberRepository.java
@@ -3,5 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.climbing.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.climbing.entities.ClimberEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ClimberRepository extends MongoRepository<ClimberEntity, String> {
+    Optional<ClimberEntity> findByEmail(String email);
 }

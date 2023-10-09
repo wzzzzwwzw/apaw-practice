1eae99e870c9b3f26d69112df1e9da76eae803b0
Martahc <martahica@gmail.com>
2023-10-09 10:43:53 +0200

    modified client delete persistence #550

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/food_delivery/persistence/ClientPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/food_delivery/persistence/ClientPersistenceMongodb.java
@@ -2,6 +2,7 @@ package es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.daos.ClientRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.entities.ClientEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.persistence_ports.food_delivery.ClientPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -24,5 +25,9 @@ public class ClientPersistenceMongodb implements ClientPersistence {
         if(clientEntity.isPresent()){
             clientRepository.delete(clientEntity.get());
         }
+        else{
+            throw new NotFoundException("Client identty: " + identity);
+        }
+
     }
 }

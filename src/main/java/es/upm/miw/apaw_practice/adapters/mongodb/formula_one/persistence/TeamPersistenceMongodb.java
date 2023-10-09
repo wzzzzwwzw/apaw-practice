86e14522547d55f37a83cbbd86777df26ed58c7d
Oliver Fernandez <oliver.fernandez@alumnos.upm.es>
2023-10-09 21:10:11 +0200

    Formula one DELETE Team mongodb persistence and repository #509

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/formula_one/persistence/TeamPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/formula_one/persistence/TeamPersistenceMongodb.java
@@ -1,8 +1,22 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.formula_one.persistence;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.formula_one.daos.TeamRepository;
 import es.upm.miw.apaw_practice.domain.persistence_ports.formula_one.TeamPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("teamPersistence")
 public class TeamPersistenceMongodb implements TeamPersistence {
+
+    private final TeamRepository teamRepository;
+
+    @Autowired
+    public TeamPersistenceMongodb(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    @Override
+    public void delete(String teamName) {
+        this.teamRepository.deleteByTeamName(teamName);
+    }
 }

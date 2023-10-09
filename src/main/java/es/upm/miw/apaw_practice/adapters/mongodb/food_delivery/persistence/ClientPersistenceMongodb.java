7ac809fbb9d94e4ecb0f9748b591938c844ad32a
Martahc <martahica@gmail.com>
2023-10-09 09:55:59 +0200

    added client delete service #550

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/food_delivery/persistence/ClientPersistenceMongodb.java
@@ -0,0 +1,25 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.daos.ClientRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.entities.ClientEntity;
+import es.upm.miw.apaw_practice.domain.persistence_ports.food_delivery.ClientPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository("clientPersistence")
+public class ClientPersistenceMongodb implements ClientPersistence {
+
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientPersistenceMongodb(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+    @Override
+    public void delete(String identity) {
+       //
+    }
+}
--- src/main/java/es/upm/miw/apaw_practice/domain/services/food_delivery/ClientService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/food_delivery/ClientService.java
@@ -16,6 +16,6 @@ public class ClientService {
     }
 
     public void delete(String identity){
-        //
+        clientPersistence.delete(identity);
     }
 }

579682d3510a3cc972f404c97b5403b0711265c1
Mario Blanco <mario10_bd@hotmail.com>
2023-10-09 22:10:28 +0200

    obdfault better test #520

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/car_workshop/OBDFaultResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/car_workshop/OBDFaultResourceIT.java
@@ -1,10 +1,14 @@
 package es.upm.miw.apaw_practice.adapters.rest.car_workshop;
 
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.car_workshop.OBDFault;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @RestTestConfig
 public class OBDFaultResourceIT {
@@ -18,6 +22,8 @@ public class OBDFaultResourceIT {
                 .get()
                 .uri(OBDFaultResource.OBDFAULTS + OBDFaultResource.ISNOTITVSAFE)
                 .exchange()
-                .expectStatus().isEqualTo(HttpStatus.OK);
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectBodyList(OBDFault.class)
+                .value(obdFaults -> assertEquals(obdFaults.size(), 2));
     }
 }

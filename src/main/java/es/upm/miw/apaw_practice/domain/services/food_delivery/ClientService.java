5ac7fc3166fed1bc86d11519d57bb28ab847ffe9
Martahc <martahica@gmail.com>
2023-10-09 09:52:47 +0200

    added client delete end-point #550

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/food_delivery/ClientService.java
@@ -0,0 +1,21 @@
+package es.upm.miw.apaw_practice.domain.services.food_delivery;
+
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.food_delivery.ClientPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    private final ClientPersistence clientPersistence;
+
+    @Autowired
+    public ClientService(ClientPersistence clientPersistence){
+        this.clientPersistence = clientPersistence;
+    }
+
+    public void delete(String identity){
+        //
+    }
+}

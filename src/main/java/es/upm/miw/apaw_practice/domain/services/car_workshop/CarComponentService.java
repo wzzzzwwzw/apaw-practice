e8d2bdab038b9b30335f0fff28d81ff68aca3777
Mario Blanco <mario10_bd@hotmail.com>
2023-10-10 09:52:42 +0200

    carcomponent created resource #521

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/CarComponentService.java
@@ -0,0 +1,10 @@
+package es.upm.miw.apaw_practice.domain.services.car_workshop;
+
+import es.upm.miw.apaw_practice.domain.models.car_workshop.CarComponent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarComponentService {
+    public CarComponent create(CarComponent carComponent) {
+    }
+}

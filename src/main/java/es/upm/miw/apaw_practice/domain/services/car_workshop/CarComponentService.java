0a95becdb04833cd88f789d5a96f80412cc3ae41
Mario Blanco <mario10_bd@hotmail.com>
2023-10-10 10:32:06 +0200

    carcomponent added service #521

--- src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/CarComponentService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/CarComponentService.java
@@ -1,10 +1,29 @@
 package es.upm.miw.apaw_practice.domain.services.car_workshop;
 
+import es.upm.miw.apaw_practice.domain.exceptions.ConflictException;
 import es.upm.miw.apaw_practice.domain.models.car_workshop.CarComponent;
+import es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop.CarComponentPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarComponentService {
+
+    private final CarComponentPersistence carComponentPersistence;
+
+    @Autowired
+    public CarComponentService(CarComponentPersistence carComponentPersistence) {
+        this.carComponentPersistence = carComponentPersistence;
+    }
+
     public CarComponent create(CarComponent carComponent) {
+        this.assertNameNotExist(carComponent.getName());
+        return this.carComponentPersistence.create(carComponent);
+    }
+
+    public void assertNameNotExist(String name) {
+        if (this.carComponentPersistence.existsName(name)) {
+            throw new ConflictException("Name exist: " + name);
+        }
     }
 }

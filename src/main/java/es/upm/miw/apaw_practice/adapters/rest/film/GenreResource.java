83f8522e5e5014aef9fb1524e84a9e2c5f4b65b1
Feiyu <f.liu@alumnos.upm.es>
2023-10-10 23:01:54 +0200

    added genre endpoint DELETE #561

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/film/GenreResource.java
@@ -0,0 +1,26 @@
+package es.upm.miw.apaw_practice.adapters.rest.film;
+
+import es.upm.miw.apaw_practice.domain.services.film.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(GenreResource.GENRES)
+public class GenreResource {
+    static final String GENRES = "/film/genres";
+
+    static final String NAME_ID = "/{name}";
+
+    private final GenreService genreService;
+
+    @Autowired
+    public GenreResource(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    public void delete(@PathVariable String name) {
+        this.genreService.delete(name);
+    }
+}

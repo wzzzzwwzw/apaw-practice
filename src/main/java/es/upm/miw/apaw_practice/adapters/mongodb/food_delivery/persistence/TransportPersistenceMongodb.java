5b7fb3aa17e93b27cfde97a8dbaf6651fe844cff
Martahc <martahica@gmail.com>
2023-10-10 15:30:32 +0200

    added transport put persistence #567

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/food_delivery/persistence/TransportPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/food_delivery/persistence/TransportPersistenceMongodb.java
@@ -2,6 +2,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.persistence;
 
 
 import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.daos.TransportRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.food_delivery.entities.TransportEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.food_delivery.Transport;
 import es.upm.miw.apaw_practice.domain.persistence_ports.food_delivery.TransportPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +15,25 @@ public class TransportPersistenceMongodb implements TransportPersistence {
     private final TransportRepository transportRepository;
 
     @Autowired
-    public TransportPersistenceMongodb(TransportRepository transportRepository){
+    public TransportPersistenceMongodb(TransportRepository transportRepository) {
         this.transportRepository = transportRepository;
     }
+
     @Override
     public Transport readByCode(String code) {
-        return null;
+        return this.transportRepository
+                .findByCode(code)
+                .orElseThrow(() -> new NotFoundException("Transport code:" + code))
+                .toTransport();
     }
 
     @Override
     public Transport updateTransportAvalaible(Transport transport) {
-        return null;
+        TransportEntity transportEntity = this.transportRepository
+                .findByCode(transport.getCode())
+                .orElseThrow(() -> new NotFoundException("Transport code: " + transport.getCode()));
+        transportEntity.setAvalaible(transport.getAvalaible());
+        return this.transportRepository.save(transportEntity).toTransport();
+
     }
 }

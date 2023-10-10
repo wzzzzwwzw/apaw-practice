748a7a635bec3004f8657d09753a1ba1471e2c97
Mario Blanco <mario10_bd@hotmail.com>
2023-10-10 17:43:28 +0200

    Invoices added service #526
    Signed-off-by: Mario Blanco <mario10_bd@hotmail.com>

--- src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/InvoiceService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/InvoiceService.java
@@ -1,9 +1,20 @@
 package es.upm.miw.apaw_practice.domain.services.car_workshop;
 
+import es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop.InvoicePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InvoiceService {
+
+    private final InvoicePersistence invoicePersistence;
+    @Autowired
+    public InvoiceService(InvoicePersistence invoicePersistence) {
+        this.invoicePersistence = invoicePersistence;
+    }
+
+
     public void deleteByPaid(Boolean paid) {
+        this.invoicePersistence.deleteByPaid(paid);
     }
 }

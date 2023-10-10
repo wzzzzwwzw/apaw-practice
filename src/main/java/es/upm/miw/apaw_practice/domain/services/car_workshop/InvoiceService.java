1c23b75561a86929e17c9fe7b95431d119116cac
Mario Blanco <mario10_bd@hotmail.com>
2023-10-10 17:40:29 +0200

    Invoices added resource #526
    Signed-off-by: Mario Blanco <mario10_bd@hotmail.com>

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/InvoiceService.java
@@ -0,0 +1,9 @@
+package es.upm.miw.apaw_practice.domain.services.car_workshop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceService {
+    public void deleteByPaid(Boolean paid) {
+    }
+}

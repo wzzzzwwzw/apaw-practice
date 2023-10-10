d618d628935692e3473e1c70bd655e0f0ccc32fc
Mario Blanco <mario10_bd@hotmail.com>
2023-10-10 10:44:17 +0200

    carcomponent added persistence #521

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/CarComponentRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/CarComponentRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.CarComponentEntity;
 
+import java.util.Optional;
+
 public interface CarComponentRepository extends MongoRepository<CarComponentEntity, String> {
 
+    Optional<CarComponentEntity> findByName(String name);
 }

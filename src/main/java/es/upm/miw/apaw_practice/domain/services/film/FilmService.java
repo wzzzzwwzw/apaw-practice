8b2699e65db244efec737aa3d2e46f6d949b4344
Feiyu <f.liu@alumnos.upm.es>
2023-10-10 15:43:12 +0200

    added review endpoint PUT #560

--- src/main/java/es/upm/miw/apaw_practice/domain/services/film/FilmService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/film/FilmService.java
@@ -1,10 +1,13 @@
 package es.upm.miw.apaw_practice.domain.services.film;
 
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.film.Film;
+import es.upm.miw.apaw_practice.domain.models.film.Review;
 import es.upm.miw.apaw_practice.domain.persistence_ports.film.FilmPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 @Service
@@ -19,5 +22,14 @@ public class FilmService {
 
     public Stream<Film> read(String title) {
         return this.filmPersistence.readByTitle(title);
+    }
+
+    public Film updateReviews(String title, List<Review> reviewList) {
+        Film film = this.filmPersistence.readByTitle(title)
+                .findFirst().orElseThrow(
+                        () -> new NotFoundException("Film title : " + title)
+                );
+        film.setReviews(reviewList);
+        return this.filmPersistence.update(film);
     }
 }

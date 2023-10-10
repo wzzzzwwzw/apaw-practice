160a5a2de5f31812316d675cb107f01b65b8fdb8
Mario Blanco <mario10_bd@hotmail.com>
2023-10-10 11:59:22 +0200

    CarToRepair added persistence #523

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/CarToRepairRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/CarToRepairRepository.java
@@ -1,8 +1,12 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
 
+import es.upm.miw.apaw_practice.domain.models.car_workshop.CarToRepair;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.CarToRepairEntity;
 
+import java.util.Optional;
+
 public interface CarToRepairRepository extends MongoRepository<CarToRepairEntity, String> {
+    Optional<CarToRepairEntity> findByRegistrationNumber(String registrationNumber);
 }

b76678dc20ee05296e2cddf25356aee11c9959f4
Mario Blanco <mario10_bd@hotmail.com>
2023-10-11 11:15:03 +0200

    OBDFault patch persistence #529

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/OBDFaultPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/OBDFaultPersistenceMongodb.java
@@ -2,8 +2,10 @@ package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.OBDFaultRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OBDFaultEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.car_workshop.OBDFault;
 import es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop.OBDFaultPersistence;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.stream.Stream;
@@ -22,5 +24,17 @@ public class OBDFaultPersistenceMongodb implements OBDFaultPersistence {
         return this.obdFaultRepository.findAll().stream()
                 .filter(obdFaultEntity -> obdFaultEntity.getIsITVSafe().equals(isITVSafe))
                 .map(OBDFaultEntity::toOBDFault);
+    }
+
+    private OBDFaultEntity readByCode(String code) {
+        return this.obdFaultRepository.findByCode(code)
+                .orElseThrow(() -> new NotFoundException("Code not found: " + code));
+    }
+
+    @Override
+    public OBDFault updatePartial(String code, OBDFault obdFault) {
+        OBDFaultEntity existingOBDFault = this.readByCode(code);
+        BeanUtils.copyProperties(obdFault, existingOBDFault);
+        return this.obdFaultRepository.save(existingOBDFault).toOBDFault();
     }
 }

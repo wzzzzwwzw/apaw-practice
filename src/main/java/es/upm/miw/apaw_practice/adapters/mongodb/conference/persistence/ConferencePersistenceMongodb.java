de2c01d1ba4ace766fc27175e6ac2a14294aa06e
JorgeDSDP <jorge.dasilva@alumni.mondragon.edu>
2023-10-11 20:55:40 +0200

    Added tests for validating Conference #570

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/conference/persistence/ConferencePersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/conference/persistence/ConferencePersistenceMongodb.java
@@ -1,9 +1,13 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.conference.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.conference.daos.ConferenceRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.conference.entities.ConferenceEntity;
+import es.upm.miw.apaw_practice.domain.models.conference.Conference;
 import es.upm.miw.apaw_practice.domain.persistence_ports.conference.ConferencePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
 
 @Repository("conferencePersistence")
 public class ConferencePersistenceMongodb implements ConferencePersistence {
@@ -17,5 +21,11 @@ public class ConferencePersistenceMongodb implements ConferencePersistence {
     @Override
     public void delete(String id) {
         this.conferenceRepository.deleteById(id);
+    }
+
+    @Override
+    public Stream<Conference> readAll() {
+        return this.conferenceRepository.findAll().stream()
+                .map(ConferenceEntity::toConference);
     }
 }

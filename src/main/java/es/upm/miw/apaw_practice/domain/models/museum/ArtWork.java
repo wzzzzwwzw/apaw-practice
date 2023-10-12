f9e24cfa6441c788b66b2166e7c6e18e8d0e7ea8
Diego Guerrero <diego.guerreroc@alumnos.upm.es>
2023-10-12 22:37:41 +0200

    Museum - Implements Builder pattern in ArtWork domain model class #654

--- src/main/java/es/upm/miw/apaw_practice/domain/models/museum/ArtWork.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/museum/ArtWork.java
@@ -17,6 +17,10 @@ public class ArtWork {
         this.room = room;
     }
 
+    public ArtWorkBuilder.InventoryNumber builder() {
+        return new ArtWork.Builder();
+    }
+
     public String getInventoryNumber() {
         return inventoryNumber;
     }
@@ -55,5 +59,72 @@ public class ArtWork {
 
     public void setRoom(Room room) {
         this.room = room;
+    }
+
+
+    private interface ArtWorkBuilder {
+        interface InventoryNumber {
+            Title inventoryNumber(String inventoryNumber);
+        }
+
+        interface Title {
+            ApproximateYear title(String title);
+        }
+
+        interface ApproximateYear {
+            Exhibited approximateYear(Integer approximateYear);
+        }
+
+        interface Exhibited {
+            RoomBuilder exhibited(Boolean exhibited);
+        }
+
+        interface RoomBuilder {
+            RoomBuilder room(Room room);
+            ArtWork build();
+        }
+    }
+
+    private static class Builder implements ArtWorkBuilder.InventoryNumber, ArtWorkBuilder.Title, ArtWorkBuilder.ApproximateYear, ArtWorkBuilder.Exhibited, ArtWorkBuilder.RoomBuilder {
+        private final ArtWork artWork;
+
+        public Builder() {
+            this.artWork = new ArtWork();
+        }
+
+        @Override
+        public ArtWorkBuilder.Title inventoryNumber(String inventoryNumber) {
+            this.artWork.setInventoryNumber(inventoryNumber);
+            return this;
+        }
+
+        @Override
+        public ArtWorkBuilder.ApproximateYear title(String title) {
+            this.artWork.setTitle(title);
+            return this;
+        }
+
+        @Override
+        public ArtWorkBuilder.Exhibited approximateYear(Integer approximateYear) {
+            this.artWork.setApproximateYear(approximateYear);
+            return this;
+        }
+
+        @Override
+        public ArtWorkBuilder.RoomBuilder exhibited(Boolean exhibited) {
+            this.artWork.setExhibited(exhibited);
+            return this;
+        }
+
+        @Override
+        public ArtWorkBuilder.RoomBuilder room(Room room) {
+            this.artWork.setRoom(room);
+            return this;
+        }
+
+        @Override
+        public ArtWork build() {
+            return this.artWork;
+        }
     }
 }

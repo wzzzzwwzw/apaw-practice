e99380f48da46a1e3d0f4b124b278c677ae6c418
dreynes <d.reynes@alumnos.upm.es>
2023-10-12 14:38:22 +0200

    Create Persistence Post Endpoint issue #623

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/aiport/FlightPersistence.java
@@ -0,0 +1,11 @@
+package es.upm.miw.apaw_practice.domain.persistence_ports.aiport;
+
+import es.upm.miw.apaw_practice.domain.models.airport.Flight;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+@Repository
+public interface FlightPersistence {
+    Flight create(Flight flight);
+}

6504a6f4d8fac71f428aa0fdfbd128ff02d0152c
Diego Guerrero <diego.guerreroc@alumnos.upm.es>
2023-10-12 12:44:10 +0200

    Museum - Registers new DELETE endpoint for Exhibition #539

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/museum/ExhibitionService.java
@@ -0,0 +1,20 @@
+package es.upm.miw.apaw_practice.domain.services.museum;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.museum.ExhibitionPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExhibitionService {
+
+    private final ExhibitionPersistence exhibitionPersistence;
+
+    @Autowired
+    public ExhibitionService(ExhibitionPersistence exhibitionPersistence) {
+        this.exhibitionPersistence = exhibitionPersistence;
+    }
+
+    public void delete(String exhibitionName) {
+        // TODO
+    }
+}

5caa33db8d5aa58b0a9eb56d2fa326e932746ba6
jiaan <chengjiaan123@gmail.com>
2023-10-13 23:16:35 +0200

    Added DELETE endpoint #597

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/furniture_store/daos/ManagerRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/furniture_store/daos/ManagerRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface ManagerRepository extends MongoRepository<ManagerEntity, String> {
     Optional<ManagerEntity> findByAffiliationNumber(String affiliationNumber);
+    void deleteByName(String name);
 }

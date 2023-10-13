6a327f5d848e7132fe7b6394381b11a07cbdb549
JunjieLi <dshen123456@gmail.com>
2023-10-13 18:36:49 +0200

    added update endpoint #645

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/coffee_shop/CoffeePersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/coffee_shop/CoffeePersistence.java
@@ -4,4 +4,5 @@ import es.upm.miw.apaw_practice.domain.models.coffee_shop.Coffee;
 
 public interface CoffeePersistence {
     Coffee read(String name);
+    Coffee update(Coffee coffee);
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/coffee_shop/persistence/CoffeePersistenceMongodbIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/coffee_shop/persistence/CoffeePersistenceMongodbIT.java
@@ -2,8 +2,11 @@ package es.upm.miw.apaw_practice.adapters.mongodb.coffee_shop.persistence;
 
 import es.upm.miw.apaw_practice.TestConfig;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.models.coffee_shop.Coffee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 @TestConfig
@@ -19,6 +22,13 @@ public class CoffeePersistenceMongodbIT {
     @Test
     void testReadFound() {
         assertNotNull(this.coffeePersistence.read("caffé americano"));
+    }
+
+    @Test
+    void testUpdate() {
+        Coffee coffee = this.coffeePersistence.update(new Coffee("Mocha Frappuccino","Tea",new BigDecimal("6.50")));
+        assertNotNull(coffee);
+        assertEquals(new BigDecimal("6.50"), coffee.getPrice());
     }
 
 }

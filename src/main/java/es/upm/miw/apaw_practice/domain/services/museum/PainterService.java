87877a9d9f9cd72fa4ad1c9d6ac0712897518c2c
Diego Guerrero <diego.guerreroc@alumnos.upm.es>
2023-10-13 12:44:50 +0200

    Museum - Implements Painter names' search endpoint #653

--- src/main/java/es/upm/miw/apaw_practice/domain/services/museum/PainterService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/museum/PainterService.java
@@ -1,9 +1,12 @@
 package es.upm.miw.apaw_practice.domain.services.museum;
 
 import es.upm.miw.apaw_practice.domain.models.museum.Painter;
+import es.upm.miw.apaw_practice.domain.persistence_ports.museum.ArtWorkPersistence;
 import es.upm.miw.apaw_practice.domain.persistence_ports.museum.PainterPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
 
 @Service
 public class PainterService {
@@ -13,6 +16,12 @@ public class PainterService {
     @Autowired
     public PainterService(PainterPersistence painterPersistence) {
         this.painterPersistence = painterPersistence;
+    }
+
+    public Stream<Painter> findByArtWorkInRoomFloorGreaterThan(Integer roomFloor) {
+        return this.painterPersistence.findAll()
+                .filter(painter -> painter.getArtWorks().stream()
+                        .anyMatch(artWork -> artWork.getRoom().getFloor() > roomFloor));
     }
 
     public Painter update(String surname, Painter painter) {

3bb277d2d7505a4ec6b2ed11a213122102105de4
jiaan <chengjiaan123@gmail.com>
2023-10-14 19:56:35 +0200

    Added POST endpoint #598

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/furniture_store/MaterialPersistence.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw_practice.domain.persistence_ports.furniture_store;
+
+import es.upm.miw.apaw_practice.domain.models.furniture_store.Material;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialPersistence {
+
+    Material create(Material material);
+    boolean existName(String barcode);
+
+}

8a01ed4268477a6fc3ce60eae8ceeba2cc54cfdc
jinyifan <563503448@qq.com>
2023-10-15 22:51:42 +0200

    fix code smells and bug #684

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/library/daos/BookRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/library/daos/BookRepositoryIT.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestConfig
-public class BookRepositoryIT {
+class BookRepositoryIT {
     @Autowired
     private BookRepository bookRepository;
 

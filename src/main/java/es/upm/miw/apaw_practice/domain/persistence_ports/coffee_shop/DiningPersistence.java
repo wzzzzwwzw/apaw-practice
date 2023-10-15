1ccaa4c44e69987a65742738d5da41abffa2d9e2
JunjieLi <dshen123456@gmail.com>
2023-10-15 16:53:00 +0200

    fixed post endpoint and tests #644

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/coffee_shop/DiningPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/coffee_shop/DiningPersistence.java
@@ -3,5 +3,6 @@ package es.upm.miw.apaw_practice.domain.persistence_ports.coffee_shop;
 import es.upm.miw.apaw_practice.domain.models.coffee_shop.Dining;
 
 public interface DiningPersistence {
+    boolean existDiningNumber(String dining);
     Dining create(Dining dining);
 }
--- src/test/java/es/upm/miw/apaw_practice/domain/services/coffee_shop/DiningServiceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/coffee_shop/DiningServiceIT.java
@@ -1,22 +1,29 @@
 package es.upm.miw.apaw_practice.domain.services.coffee_shop;
 
 import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.domain.exceptions.ConflictException;
 import es.upm.miw.apaw_practice.domain.models.coffee_shop.Dining;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestConfig
 public class DiningServiceIT {
     @Autowired
     private DiningService diningService;
     @Test
     void testCreateDining() {
-        Dining dining = this.diningService.Create(new Dining("1","location1",6));
+        Dining dining = this.diningService.Create(new Dining("CREATE_TEST","CREATE_TEST",6));
         assertNotNull(dining);
-        assertEquals("1", dining.getDiningNumber());
-        assertEquals("location1", dining.getLocation());
+        assertEquals("CREATE_TEST", dining.getDiningNumber());
+        assertEquals("CREATE_TEST", dining.getLocation());
         assertEquals(6, dining.getCapacity());
+    }
+
+    @Test
+    void testCreateAlreadyExistsDining() {
+        Dining dining = new Dining("1","location1",6);
+        assertThrows(ConflictException.class, () -> this.diningService.Create(dining));
     }
 }

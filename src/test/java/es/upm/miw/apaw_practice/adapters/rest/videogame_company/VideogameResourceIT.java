8909fc1bc5c7b8560afe324680813c50b146722e
ricardo.vente <ricardo751991@gmail.com>
2023-10-16 01:41:06 +0200

    add tests for PATCH endpoint #637

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/videogame_company/VideogameResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/videogame_company/VideogameResourceIT.java
@@ -2,10 +2,18 @@ package es.upm.miw.apaw_practice.adapters.rest.videogame_company;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.videogame_company.VideogameCompanySeederService;
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.videogame_company.Videogame;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @RestTestConfig
 public class VideogameResourceIT {
@@ -29,6 +37,24 @@ public class VideogameResourceIT {
                 .uri(VideogameResource.VIDEOGAMES + VideogameResource.NAME_ID, "Pyre")
                 .exchange()
                 .expectStatus().isOk();
+    }
+
+    @Test
+    void testPatch(){
+        this.webTestClient
+                .patch()
+                .uri(VideogameResource.VIDEOGAMES + VideogameResource.NAME_ID, "Super Bomberman R")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Videogame.class)
+                .value(Assertions::assertNotNull)
+                .value(
+                        videogame -> {
+                            assertEquals("Super Bomberman R", videogame.getName());
+                            assertTrue(videogame.getReleaseDate().atStartOfDay()
+                                    .isEqual(LocalDate.now().atStartOfDay()));
+                        }
+                );
     }
 
 }
--- src/test/java/es/upm/miw/apaw_practice/domain/services/videogame_company/VideogameServiceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/videogame_company/VideogameServiceIT.java
@@ -1,8 +1,14 @@
 package es.upm.miw.apaw_practice.domain.services.videogame_company;
 
 import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.models.videogame_company.Videogame;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 public class VideogameServiceIT {
@@ -13,5 +19,16 @@ public class VideogameServiceIT {
     @Test
     void testDelete(){
         videogameService.delete("To the Moon");
+    }
+
+    @Test
+    void testUpdateReleaseDateByName(){
+        Videogame videogame = videogameService.updateReleaseDateByName("Super Bomberman R");
+        assertEquals("Super Bomberman R", videogame.getName());
+        assertTrue(videogame.getReleaseDate().atStartOfDay().isEqual(LocalDate.now().atStartOfDay()));
+    }
+
+    void testUpdateReleaseDateByNameNotFound(){
+        assertThrows(NotFoundException.class, () -> this.videogameService.updateReleaseDateByName("x"));
     }
 }

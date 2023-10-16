0dfe320720f37c53d94893e4cb736ecf372f6978
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2023-10-16 18:02:43 +0200

    Code cleanup #617

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/school/SubjectResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/school/SubjectResource.java
@@ -5,8 +5,6 @@ import es.upm.miw.apaw_practice.domain.services.school.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(SubjectResource.SUBJECTS)
 public class SubjectResource {

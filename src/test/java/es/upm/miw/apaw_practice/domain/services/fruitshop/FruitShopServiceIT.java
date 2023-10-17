34cf0cbc2e83307a45b1e9494b56c5f28083bc13
Meng80 <metang@alumnos.upm.es>
2023-10-17 16:48:59 +0200

    added search2 #686

--- src/test/java/es/upm/miw/apaw_practice/domain/services/fruitshop/FruitShopServiceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/fruitshop/FruitShopServiceIT.java
@@ -1,16 +1,41 @@
 package es.upm.miw.apaw_practice.domain.services.fruitshop;
 
 import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.fruitshop.FruitShopSeederService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestConfig
 public class FruitShopServiceIT {
     @Autowired
     private  FruitShopService fruitShopService;
 
+    @Autowired
+    private FruitShopSeederService fruitShopSeederService;
+
+    @BeforeEach
+    void cleanUp(){
+        this.fruitShopSeederService.deleteAll();
+        this.fruitShopSeederService.seedDatabase();
+    }
+
     @Test
     void testDelete() {
         fruitShopService.delete("Fruitful Delights");
     }
+
+    @Test
+    void testFindAddressByFruitSpeice() {
+        List<String> specie = new LinkedList<>();
+        specie.add("Malaga");
+        assertEquals(specie,this.fruitShopService.findAddressByFruitSpecie("Normal").toList());
+    }
+
+
 }

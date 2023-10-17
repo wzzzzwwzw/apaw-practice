6c5dd4f51c8150797786a9d20b3ad978856b1993
Hugo Vázquez Docampo <81679649+hugovazquezd@users.noreply.github.com>
2023-10-17 09:59:44 +0200

    Created PUT - CampaignService issue #720

--- src/main/java/es/upm/miw/apaw_practice/domain/services/influencer_agency/CampaignService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/influencer_agency/CampaignService.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw_practice.domain.services.influencer_agency;
 
 import es.upm.miw.apaw_practice.domain.models.influencer_agency.Campaign;
+import es.upm.miw.apaw_practice.domain.models.influencer_agency.Content;
 import es.upm.miw.apaw_practice.domain.persistence_ports.influencer_agency.CampaignPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,12 @@ public class CampaignService {
 
     public Campaign create(Campaign campaign) {
         return this.campaignPersistence.create(campaign);
+    }
+
+    public void updateContentForCampaign(String campaignDescription, Content content) {
+        Campaign campaign = campaignPersistence.findByDescription(campaignDescription);
+        campaign.getContents().add(content);
+
+        campaignPersistence.save(campaign);
     }
 }

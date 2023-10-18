50a72b800180566a92e1f348809d65bd50e2e0bd
jiaan <chengjiaan123@gmail.com>
2023-10-18 23:08:02 +0200

    Added First Search and test #733

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/furniture_store/MaterialResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/furniture_store/MaterialResource.java
@@ -1,17 +1,22 @@
 package es.upm.miw.apaw_practice.adapters.rest.furniture_store;
 
+import es.upm.miw.apaw_practice.adapters.rest.LexicalAnalyzer;
 import es.upm.miw.apaw_practice.domain.models.furniture_store.Material;
 import es.upm.miw.apaw_practice.domain.services.furniture_store.MaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(MaterialResource.MATERIAL)
+@RequestMapping(MaterialResource.MATERIALS)
 public class MaterialResource {
 
-    static final String MATERIAL = "/furniture-store/materials";
-
     private final MaterialService materialService;
+
+    static final String MATERIALS = "/furniture-store/materials";
+
+    static final String SEARCH = "/search";
 
     @Autowired
     public MaterialResource(MaterialService materialService) {
@@ -22,4 +27,11 @@ public class MaterialResource {
     public Material create(@RequestBody Material material) {
         return this.materialService.create(material);
     }
+
+    @GetMapping(SEARCH)
+    public List<String> findUniqueMaterialTypeByManagerPromotionCandidate(@RequestParam String q) {
+        Boolean promotionCandidate = Boolean.valueOf(new LexicalAnalyzer().extractWithAssure(q, "promotion-candidate"));
+        return this.materialService.findUniqueMaterialTypeByManagerPromotionCandidate(promotionCandidate);
+    }
+
 }

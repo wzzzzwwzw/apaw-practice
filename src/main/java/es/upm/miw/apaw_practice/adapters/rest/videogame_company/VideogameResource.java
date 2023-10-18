4303d260beaee7803c72f546414ea13fdf7755fc
ricardo.vente <ricardo751991@gmail.com>
2023-10-18 16:50:01 +0200

    add second search and remove videogame seeder comments #726

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame_company/VideogameResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame_company/VideogameResource.java
@@ -1,16 +1,21 @@
 package es.upm.miw.apaw_practice.adapters.rest.videogame_company;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.videogame_company.daos.VideogameRepository;
+import es.upm.miw.apaw_practice.adapters.rest.LexicalAnalyzer;
 import es.upm.miw.apaw_practice.domain.models.videogame_company.Videogame;
 import es.upm.miw.apaw_practice.domain.services.videogame_company.VideogameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(VideogameResource.VIDEOGAMES)
 public class VideogameResource {
     static final String VIDEOGAMES = "/videogame-company/videogames";
     static final String NAME_ID = "/{name}";
+
+    static final String SEARCH = "/search";
 
     @Autowired
     private  final VideogameService videogameService;
@@ -27,6 +32,12 @@ public class VideogameResource {
     @PatchMapping(NAME_ID)
     public Videogame updateReleaseDateByName(@PathVariable String name){
         return this.videogameService.updateReleaseDateByName(name);
+    }
+
+    @GetMapping(SEARCH)
+    public BigDecimal findSumOfVideogamePricesByGameEngineLicense(@RequestParam String q){
+        String license = new LexicalAnalyzer().extractWithAssure(q, "license", String::new);
+        return this.videogameService.findSumOfVideogamePricesByGameEngineLicence(license);
     }
 
 }
--- src/main/java/es/upm/miw/apaw_practice/domain/services/videogame_company/VideogameService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/videogame_company/VideogameService.java
@@ -1,9 +1,15 @@
 package es.upm.miw.apaw_practice.domain.services.videogame_company;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.videogame_company.entities.GameEngineEntity;
+import es.upm.miw.apaw_practice.domain.models.videogame_company.GameEngine;
 import es.upm.miw.apaw_practice.domain.models.videogame_company.Videogame;
 import es.upm.miw.apaw_practice.domain.persistence_ports.videogame_company.VideogamePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class VideogameService {
@@ -20,5 +26,14 @@ public class VideogameService {
 
     public Videogame updateReleaseDateByName(String name){
         return this.videogamePersistence.updateReleaseDateByName(name);
+    }
+
+    public BigDecimal findSumOfVideogamePricesByGameEngineLicence(String license){
+        return this.videogamePersistence.readAll()
+                .filter(videogame -> videogame.getGameEngine().getLicense().equals(license)
+                )
+                .map(Videogame::getPrice)
+                .reduce((accumulatorPrice, currentPrice) -> currentPrice.add(accumulatorPrice))
+                .orElse(BigDecimal.ZERO);
     }
 }

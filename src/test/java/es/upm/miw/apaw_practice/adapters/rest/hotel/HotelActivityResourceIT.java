81255913296e6e7c084b8bc64a11d3eb948f7b2f
Leonardo Alfonso Cruz Rodríguez <alu0101233093@ull.edu.es>
2023-10-18 19:28:41 +0200

    Deleted unused imports #750

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/hotel/HotelActivityResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/hotel/HotelActivityResourceIT.java
@@ -1,18 +1,12 @@
 package es.upm.miw.apaw_practice.adapters.rest.hotel;
 
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
-import es.upm.miw.apaw_practice.domain.models.hospital.Department;
 import es.upm.miw.apaw_practice.domain.models.hotel.HotelActivity;
-import es.upm.miw.apaw_practice.domain.models.hotel.HotelBooking;
-import es.upm.miw.apaw_practice.domain.models.hotel.HotelClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 

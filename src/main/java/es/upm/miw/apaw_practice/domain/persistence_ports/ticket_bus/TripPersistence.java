ba5b30052c71d23eefca4751b66bd4ff1ab04183
Wael Louati <46055260+wzzzzwwzw@users.noreply.github.com>
2023-10-18 13:27:01 +0200

    Added Search 1

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/ticket_bus/TripPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/ticket_bus/TripPersistence.java
@@ -3,7 +3,10 @@ package es.upm.miw.apaw_practice.domain.persistence_ports.ticket_bus;
 import es.upm.miw.apaw_practice.domain.models.ticket_bus.Trip;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+
 @Repository
 public interface TripPersistence {
     Trip readByPath(String path);
+    BigDecimal getSumOfTicketPricesByTripPath(String path);
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/ticket_bus/TripResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/ticket_bus/TripResourceIT.java
@@ -4,7 +4,14 @@ import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
 import es.upm.miw.apaw_practice.domain.models.ticket_bus.Trip;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @RestTestConfig
 public class TripResourceIT {
     @Autowired
@@ -19,5 +26,20 @@ public class TripResourceIT {
                 .expectStatus().isOk()
                 .expectBody(Trip.class);
 
+    }
+
+    @Test
+    void testGetSumOfTicketPricesByPath() {
+        EntityExchangeResult<BigDecimal> result = this.webTestClient
+                .get()
+                .uri(TripResource.TRIPS + TripResource.PATH + "/total-price", "Madrid-Toledo")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BigDecimal.class)
+                .returnResult();
+
+        BigDecimal totalPrice = result.getResponseBody();
+        assertNotNull(totalPrice);
+        assertTrue(totalPrice.compareTo(BigDecimal.ZERO) >= 0);
     }
 }

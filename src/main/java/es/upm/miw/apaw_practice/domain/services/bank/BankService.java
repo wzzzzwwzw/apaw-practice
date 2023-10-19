6c43bc3a809fcb713e90d2aa65c58235f3574e3e
Jorge <jorge.mpavon@alumnos.upm.es>
2023-10-20 01:48:47 +0200

    added functions to BankService and BankResource for PATCH EndPoint issue #766

--- src/main/java/es/upm/miw/apaw_practice/domain/services/bank/BankService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/bank/BankService.java
@@ -1,6 +1,8 @@
 package es.upm.miw.apaw_practice.domain.services.bank;
 
+import es.upm.miw.apaw_practice.adapters.rest.bank.dto.IncrementBalanceDto;
 import es.upm.miw.apaw_practice.domain.models.bank.Bank;
+import es.upm.miw.apaw_practice.domain.models.bank.BankAccount;
 import es.upm.miw.apaw_practice.domain.persistence_ports.bank.BankPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +30,9 @@ public class BankService {
         Bank bank=this.bankPersistence.readByBankName(bankName);
         bank.setCapital(capital);
         return this.bankPersistence.updateBankCapital(bank);
+    }
+
+    public BankAccount updateIncreaseBankAccountBalance(String bankName, IncrementBalanceDto bodyIncrement){
+        return this.bankPersistence.updateIncreaseBankAccountBalance(bankName,bodyIncrement);
     }
 }

4e0e90f4cae123dc8c4f7dbdeffe661489deffbe
jiaan <chengjiaan123@gmail.com>
2023-10-19 19:10:14 +0200

    Added Second Search #734

--- src/main/java/es/upm/miw/apaw_practice/domain/services/furniture_store/FurnitureStoreService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/furniture_store/FurnitureStoreService.java
@@ -1,11 +1,14 @@
 package es.upm.miw.apaw_practice.domain.services.furniture_store;
 
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.furniture_store.Furniture;
 import es.upm.miw.apaw_practice.domain.models.furniture_store.FurnitureStore;
 import es.upm.miw.apaw_practice.domain.persistence_ports.furniture_store.FurnitureStorePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Service
@@ -26,6 +29,20 @@ public class FurnitureStoreService {
         FurnitureStore furnitureStore = this.furnitureStorePersistence.readByName(name);
         furnitureStore.setFurnitures(furnitureList);
         return this.furnitureStorePersistence.update(furnitureStore);
+    }
+
+    public BigDecimal findAverageFurniturePriceByManagerName(String name) {
+        List<Furniture> furnitureList = this.furnitureStorePersistence.findFurnituresByManagerName(name);
+
+        if (furnitureList.isEmpty()) {
+            throw new NotFoundException("No furniture found with " + name + " manager name");
+        }
+
+        return furnitureList
+                .stream()
+                .map(Furniture::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .divide(BigDecimal.valueOf(furnitureList.size()), RoundingMode.UNNECESSARY);
     }
 
 }

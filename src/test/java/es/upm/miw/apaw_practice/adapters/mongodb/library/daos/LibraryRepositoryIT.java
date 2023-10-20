b72f4a368bd83a92f276fd550416b7668af68aab
jinyifan <563503448@qq.com>
2023-10-20 13:35:13 +0200

    fix to improve coverage #issue 781

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/library/daos/LibraryRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/library/daos/LibraryRepositoryIT.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,5 +29,22 @@ class LibraryRepositoryIT {
                 .map(BookEntity::getIsbn)
                 .toList()
                 .containsAll(Arrays.asList("9788401020414", "9788416738090")));
+    }
+    @Test
+    void testLibraryEntity(){
+        assertTrue(this.libraryRepository.findByName("Biblioteca Nacional").isPresent());
+        LibraryEntity library = this.libraryRepository.findByName("Biblioteca Nacional").get();
+        LibraryEntity newLibrary = new LibraryEntity("Biblioteca","Calle",LocalDate.of(2000,10,20), library.getBookEntities());
+        newLibrary.setName("Biblioteca test");
+        assertEquals("Biblioteca test", newLibrary.getName());
+        newLibrary.setAddress("Calle test");
+        assertEquals("Calle test", newLibrary.getAddress());
+        newLibrary.setFoundationDate(LocalDate.of(1999,1,1));
+        assertEquals(LocalDate.of(1999,1,1), newLibrary.getFoundationDate());
+
+        assertTrue(this.libraryRepository.findByName("Biblioteca territorial").isPresent());
+        LibraryEntity anotherLibrary = this.libraryRepository.findByName("Biblioteca territorial").get();
+        newLibrary.setBookEntities(anotherLibrary.getBookEntities());
+        assertEquals(1, newLibrary.getBookEntities().size());
     }
 }

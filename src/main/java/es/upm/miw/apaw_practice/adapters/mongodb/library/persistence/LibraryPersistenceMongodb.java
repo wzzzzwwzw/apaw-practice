62b02ec7e5c60b6dda5f3cd9dee3d32524748a3c
jinyifan <563503448@qq.com>
2023-10-20 02:13:10 +0200

    fix code smells #763

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/library/persistence/LibraryPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/library/persistence/LibraryPersistenceMongodb.java
@@ -1,7 +1,6 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.library.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.library.daos.LibraryRepository;
-import es.upm.miw.apaw_practice.adapters.mongodb.library.entities.BookEntity;
 import es.upm.miw.apaw_practice.adapters.mongodb.library.entities.BookWriterEntity;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.library.Library;

d4fec3be7a95f2e723f4bebfa8e713ab852cfe35
adriancastrovilar <adrian.castrov@alumnos.upm.es>
2023-10-20 13:55:45 +0200

    Added more service and persistence tests #661

--- src/test/java/es/upm/miw/apaw_practice/domain/services/restaurant/IngredientServiceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/restaurant/IngredientServiceIT.java
@@ -5,6 +5,8 @@ import es.upm.miw.apaw_practice.domain.models.restaurant.Ingredient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
@@ -14,11 +16,17 @@ public class IngredientServiceIT {
     private IngredientService ingredientService;
 
     @Test
-    void testFindByNameService(){
+    void testFindByNameService() {
         Ingredient ingredient = ingredientService.findByName("Pollo");
         assertNotNull(ingredient);
         assertFalse(ingredient.isSpicy());
         assertTrue(ingredient.isAvailable());
+    }
+
+    @Test
+    void testFindAllIngredientNamesByCategoryColor() {
+        List<String> ingredientNames = ingredientService.findAllIngredientNamesByCategoryColor("Azul");
+        assertEquals(List.of("Arroz", "Lubina"), ingredientNames);
     }
 
 }

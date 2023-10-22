4ebaefa77ca24021df2cad8f239531ff3b1aafcc
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2023-10-22 20:41:44 +0200

    added Search 1 findIdentifiersByPavilionDirection #788

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/basketball/BasketResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/basketball/BasketResource.java
@@ -1,14 +1,20 @@
 package es.upm.miw.apaw_practice.adapters.rest.basketball;
 
+import es.upm.miw.apaw_practice.adapters.rest.LexicalAnalyzer;
 import es.upm.miw.apaw_practice.domain.models.basketball.Basket;
 import es.upm.miw.apaw_practice.domain.services.basketball.BasketService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping(BasketResource.BASKET)
 public class BasketResource {
     static final String BASKET = "/basketball/baskets";
     static final String ID = "/{id}";
     static final String UPDATE_DATA = "/update";
+    static final String SEARCH = "/search";
+
     private final BasketService basketService;
 
     public BasketResource(BasketService basketService) {
@@ -18,5 +24,12 @@ public class BasketResource {
     @PatchMapping(ID + UPDATE_DATA)
     public Basket update(@PathVariable String id, @RequestBody Integer value) {
         return this.basketService.updateBasket(id, value);
+    }
+
+
+    @GetMapping(SEARCH)
+    public List<String> findIdentifiersByPavilionDirection(@RequestParam String q) {
+        String direction = new LexicalAnalyzer().extractWithAssure(q, "direction");
+        return this.basketService.findIdentifiersByPavilionDirection(direction);
     }
 }
--- src/main/java/es/upm/miw/apaw_practice/domain/services/basketball/BasketService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/basketball/BasketService.java
@@ -1,15 +1,21 @@
 package es.upm.miw.apaw_practice.domain.services.basketball;
 
 import es.upm.miw.apaw_practice.domain.models.basketball.Basket;
+import es.upm.miw.apaw_practice.domain.models.basketball.Player;
 import es.upm.miw.apaw_practice.domain.persistence_ports.basketball.BasketPersistence;
+import es.upm.miw.apaw_practice.domain.persistence_ports.basketball.TeamPersistence;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BasketService {
     private final BasketPersistence basketPersistence;
+    private final TeamPersistence teamPersistence;
 
-    public BasketService(BasketPersistence basketPersistence) {
+    public BasketService(BasketPersistence basketPersistence, TeamPersistence teamPersistence) {
         this.basketPersistence = basketPersistence;
+        this.teamPersistence = teamPersistence;
     }
 
     public Basket read(String id) {
@@ -21,4 +27,22 @@ public class BasketService {
         basket.setValue(value);
         return this.basketPersistence.update(basket);
     }
+
+    public List<String> findIdentifiersByPavilionDirection(String direction) {
+
+        List<Player> teamPlayers = this.teamPersistence.findAll()
+                .stream()
+                .filter(team -> team.getPavilions().stream().anyMatch(pavilion -> pavilion.getDirection().equals(direction)))
+                .flatMap(team -> team.getPlayers().stream())
+                .distinct()
+                .toList();
+
+        return this.basketPersistence.findAll()
+                .stream()
+                .filter(basket -> teamPlayers.stream()
+                        .anyMatch(teamPlayer -> teamPlayer.getEmail().equals(basket.getPlayer().getEmail())))
+                .map(Basket::getIdentifier)
+                .toList();
+    }
+
 }

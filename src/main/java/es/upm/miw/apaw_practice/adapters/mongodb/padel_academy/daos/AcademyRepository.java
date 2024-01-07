597c6ca3079209f73142dae4b43ce9bafea21563
Michael Hernandez <michaelhernandezrala@gmail.com>
2024-01-07 13:50:47 +0100

    Add DAOS IT #584

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/padel_academy/daos/AcademyRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/padel_academy/daos/AcademyRepository.java
@@ -3,5 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.padel_academy.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.padel_academy.entities.AcademyEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface AcademyRepository extends MongoRepository<AcademyEntity, String> {
+    Optional<AcademyEntity> findByName(String name);
 }
